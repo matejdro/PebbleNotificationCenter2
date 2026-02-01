459a683fbe18c6de4ab84a10f41c3e339be062bb
Matej Drobnič <matej@matejdro.com>
2026-02-01 13:03:44 +0100

    fix(logging): missing app crash

--- mobile/notification/data/src/main/kotlin/com/matejdro/pebblenotificationcenter/notification/parsing/AppNameProvider.kt
+++ mobile/notification/data/src/main/kotlin/com/matejdro/pebblenotificationcenter/notification/parsing/AppNameProvider.kt
@@ -1,6 +1,7 @@
 package com.matejdro.pebblenotificationcenter.notification.parsing
 
 import android.content.Context
+import android.content.pm.PackageManager
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesBinding
 import dev.zacsweers.metro.Inject
@@ -13,6 +14,10 @@ fun interface AppNameProvider {
 @ContributesBinding(AppScope::class)
 class AppNameProviderImpl(private val context: Context) : AppNameProvider {
    override fun getAppName(pkg: String): String {
-      return context.packageManager.getApplicationInfo(pkg, 0).loadLabel(context.packageManager).toString()
+      return try {
+         context.packageManager.getApplicationInfo(pkg, 0).loadLabel(context.packageManager).toString()
+      } catch (ignored: PackageManager.NameNotFoundException) {
+         pkg
+      }
    }
 }
