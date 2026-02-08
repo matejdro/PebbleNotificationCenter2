ebb28891f94b3ba7e3e1ff1caa5ed41c5d2dd405
Matej Drobnič <matej@matejdro.com>
2026-02-08 20:23:56 +0100

    refactor(common-navigation): remove unused MasterDetailScreen

--- mobile/common-navigation/build.gradle.kts
+++ mobile/common-navigation/build.gradle.kts
@@ -6,10 +6,7 @@ plugins {
 
 dependencies {
    api(libs.kotlinova.navigation)
-   implementation(libs.accompanist.adaptive)
    implementation(libs.androidx.activity.compose)
    implementation(libs.androidx.compose.material3.sizeClasses)
-   implementation(libs.kotlin.coroutines)
-   implementation(libs.kotlinova.core)
    implementation(libs.kotlinova.compose)
 }
