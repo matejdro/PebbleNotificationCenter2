0e7605d16931539bfddc1c43d13161e6792287cf
Matej Drobnič <matej@matejdro.com>
2026-03-24 16:41:16 +0100

    fix(common-navigation): attempt to fix popups sometimes not saving
    this is a possible fix for #4

--- mobile/common-navigation/src/main/kotlin/com/matejdro/pebblenotificationcenter/navigation/util/NavigationPopup.kt
+++ mobile/common-navigation/src/main/kotlin/com/matejdro/pebblenotificationcenter/navigation/util/NavigationPopup.kt
@@ -2,9 +2,12 @@ package com.matejdro.pebblenotificationcenter.navigation.util
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.currentCompositeKeyHashCode
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import si.inova.kotlinova.compose.result.LocalResultPassingStore
 import si.inova.kotlinova.compose.result.ResultKey
-import si.inova.kotlinova.compose.result.registerResultReceiver
 import si.inova.kotlinova.navigation.instructions.navigateTo
 import si.inova.kotlinova.navigation.navigator.Navigator
 import si.inova.kotlinova.navigation.screenkeys.DialogKey
@@ -15,7 +18,18 @@ fun <P, T : Any, K> Navigator.rememberNavigationPopup(
    navigationKey: Context.(param: P, resultKey: ResultKey<T>) -> K,
    onResult: (T) -> Unit,
 ): PopupTrigger<P> where K : ScreenKey, K : DialogKey {
-   val resultKey = registerResultReceiver(onResult)
+   val store = LocalResultPassingStore.current
+   val key = currentCompositeKeyHashCode
+
+   val resultKey = remember {
+      store.registerCallback(key, onResult)
+   }
+
+   DisposableEffect(resultKey) {
+      onDispose {
+         store.unregisterCallback(resultKey)
+      }
+   }
    val context = LocalContext.current
 
    return PopupTrigger<P> { param ->
